f5fa4aac86f66fead71c46b3fc7105492a515696
EMedved-bit <71488290+EMedved-bit@users.noreply.github.com>
2021-02-24 18:33:27 +0300

    Добавлен скрипт для формы на главной странице

---
+++ js/form.js
@@ -0,0 +1,57 @@
+const searchButton = document.querySelector(".search-button");
+const searchForm = document.querySelector(".search-form");
+const arrivalDate = searchForm.querySelector("#arrival_date");
+const depatureDate = searchForm.querySelector("#depature-date");
+const adult = searchForm.querySelector("#adult");
+const children = searchForm.querySelector("#children");
+
+let isStorageSupport = true;
+let storageAdult = "";
+let storageChildren = "";
+
+searchForm.classList.add("search-form-hidden");
+
+try {
+  storageAdult = localStorage.getItem("adult");
+  storageChildren = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storageAdult) {
+  adult.value = storageAdult;
+}
+
+if(storageChildren) {
+  children.value = storageChildren;
+}
+
+searchButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (searchForm.classList.contains("search-form-hidden")) {
+    searchForm.classList.remove("search-form-hidden");
+    arrivalDate.focus();
+  } else {
+    searchForm.classList.add("search-form-hidden");
+  }
+})
+
+searchForm.addEventListener("submit", function(event) {
+  if (!arrivalDate.value || !depatureDate.value || !adult.value) {
+    event.preventDefault();
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adult", adult.value);
+      localStorage.setItem("children", children.value);
+    }
+  }
+})
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    if (!searchForm.classList.contains("search-form-hidden")) {
+      event.preventDefault();
+      searchForm.classList.add("search-form-hidden");
+    }
+  }
+})
